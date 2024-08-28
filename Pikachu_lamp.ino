90e03e0d19278dabe8bba349ecc6703d6a59824b
Tomas Zikmund <tommekster@gmail.com>
2024-08-28 10:20:10 +0200

    pika-pi - lights animation

--- Pikachu_lamp.ino
+++ Pikachu_lamp.ino
@@ -6,10 +6,21 @@
 
 //const int repro_pin = 11;
 const int button_pin = 4;
+const int spark1_pin = 5;
+const int spark2_pin = 6;
+const int spark3_pin = 7;
+const int orange_pin = 8;
+
+void lampOn();
 
 void setup()
 {
   pinMode(button_pin, INPUT);
+  pinMode(spark1_pin, OUTPUT);
+  pinMode(spark2_pin, OUTPUT);
+  pinMode(spark3_pin, OUTPUT);
+  pinMode(orange_pin, OUTPUT);
+  lampOn();
 }
 
 void pikachu_speech();
@@ -43,10 +54,65 @@ void playOnce(unsigned char const *data, int length)
   stopPlayback();
 }
 
-void pika() { playOnce(PIKA_WAV, sizeof(PIKA_WAV)); }
-void pi() { playOnce(PI_WAV, sizeof(PI_WAV)); }
-void chu() { playOnce(CHU_WAV, sizeof(CHU_WAV)); }
-void spark() { playOnce(SPARKING_WAV, sizeof(SPARKING_WAV)); }
+void lampOn() { digitalWrite(orange_pin, LOW); }
+void lampOff() { digitalWrite(orange_pin, HIGH); }
+
+void pika()
+{
+  auto len = sizeof(PIKA_WAV);
+  startPlayback(PIKA_WAV, len);
+
+  auto startTime = millis();
+  for(auto t = 0; t < len/8; t = millis() - startTime) {
+    auto speaking = (34 < t && t < 106) || (214 < t && t < 311);
+    if(speaking) {
+      lampOff(); 
+    } else {
+      lampOn();
+    }
+    delay(5);
+  }
+  stopPlayback();
+  lampOn();
+}
+
+void pi()
+{
+  lampOff();
+  playOnce(PI_WAV, sizeof(PI_WAV));
+  lampOn();
+}
+
+void chu()
+{
+  lampOff();   
+  playOnce(CHU_WAV, sizeof(CHU_WAV)); 
+  lampOn();
+}
+
+void spark()
+{
+  auto len = sizeof(SPARKING_WAV);
+  startPlayback(SPARKING_WAV, len);
+  lampOff();
+
+  auto startTime = millis();
+  for(auto t = 0; t < len/8; t = millis() - startTime) {
+    auto color = (t / 100) % 3;
+    digitalWrite(spark1_pin, color == 0 ? HIGH : LOW);
+    digitalWrite(spark2_pin, color == 1 ? HIGH : LOW);
+    digitalWrite(spark3_pin, color == 2 ? HIGH : LOW);
+    delay(5);
+  }
+  
+  
+  
+  stopPlayback();
+  digitalWrite(spark1_pin, LOW);
+  digitalWrite(spark2_pin, LOW);
+  digitalWrite(spark3_pin, LOW);
+  lampOn();
+}
 
 void pika_pi()
 {
